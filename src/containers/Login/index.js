e90966834c06f305137c54d9cbadc71ea867483c
paulocunha31 <pauloroberto_odc@hotmail.com>
2023-10-07 14:36:47 -0300

    code burger application under development

---
+++ src/containers/Login/index.js
@@ -0,0 +1,91 @@
+import { yupResolver } from '@hookform/resolvers/yup'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { toast } from 'react-toastify'
+import * as Yup from 'yup'
+
+import Loginimage from '../../assets/login-image.svg'
+import Logo from '../../assets/logo.svg'
+import Button from '../../Components/Button'
+import api from '../../services/api'
+import {
+  Container,
+  LoginImage,
+  ContainerItens,
+  Label,
+  Input,
+  SingInLink,
+  ErrorMessage
+} from './styles'
+
+function Login() {
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .email('Digite um e-mail válido')
+      .required('O e-mail é obrigatório'),
+    password: Yup.string()
+      .required('A senha é obrigatória')
+      .min(6, 'A senha deve conter pelo menos 6 dígitos')
+  })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(schema)
+  })
+
+  const onSubmit = async clientData => {
+    const response = await toast.promise(
+      api.post('sessions', {
+        email: clientData.email,
+        password: clientData.password
+      }),
+      {
+        pending: 'Verificando seus dados',
+        success: 'Seja bem-vindo(a)',
+        error: 'Verifique seu e-mail e senha'
+      }
+    )
+    console.log(response)
+  }
+
+  return (
+    <Container>
+      <LoginImage src={Loginimage} alt="login-image" />
+      <ContainerItens>
+        <img src={Logo} alt="logo-code-burger" />
+        <h1>Login</h1>
+
+        <form noValidate onSubmit={handleSubmit(onSubmit)}>
+          <Label>Email</Label>
+          <Input
+            type="email"
+            {...register('email')}
+            error={errors.email?.message}
+          />
+          <ErrorMessage>{errors.email?.message}</ErrorMessage>
+
+          <Label>Senha</Label>
+          <Input
+            type="password"
+            {...register('password')}
+            error={errors.password?.message}
+          />
+          <ErrorMessage>{errors.password?.message}</ErrorMessage>
+
+          <Button type="submit" style={{ marginTop: 35, marginBottom: 18 }}>
+            Entrar
+          </Button>
+        </form>
+
+        <SingInLink>
+          Não possui contar ? <a>Cadastrar</a>
+        </SingInLink>
+      </ContainerItens>
+    </Container>
+  )
+}
+
+export default Login
---
+++ src/containers/Login/styles.js
@@ -0,0 +1,84 @@
+import styled from 'styled-components'
+
+import Background from '../../assets/Background.svg'
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background: url('${Background}');
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const LoginImage = styled.img`
+  height: 75%;
+  border-radius: 10px 0 0 10px;
+`
+
+export const ContainerItens = styled.div`
+  background: #373737;
+  border-radius: 0 10px 10px 0;
+  height: 75%;
+  padding: 25px 75px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  h1 {
+    color: #fff;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    text-align: center;
+    margin-top: 20px;
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+`
+
+export const Label = styled.p`
+  color: #fff;
+  width: 197.39px;
+  height: 19.707px;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  margin-top: 28px;
+  margin-bottom: 5px;
+`
+
+export const Input = styled.input`
+  border-radius: 5px;
+  background: #fff;
+  box-shadow: 3px 3px 10px 0px rgba(74, 144, 226, 0.19);
+  width: 391.416px;
+  height: 38.319px;
+  border: ${props => (props.error ? '2px solid #cc1717' : 'none')};
+  padding-left: 10px;
+`
+
+export const SingInLink = styled.p`
+  color: #fff;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 300;
+  line-height: normal;
+
+  a {
+    cursor: pointer;
+    text-decoration: underline;
+  }
+`
+export const ErrorMessage = styled.p`
+  color: #cc1717;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  margin-top: 3px;
+`
